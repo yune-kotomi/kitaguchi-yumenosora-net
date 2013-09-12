4cae6c41cff1df5131639ef180d79e7f6a0ee96d
Yune Kotomi <yune-kotomi@users.noreply.github.com>
2013-09-13 07:09:06 +0900

    初期インポート

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hotarugaike::Application.config.secret_key_base = 'c18b489b694d996b4a7bb85f4d6bc9df74d4726424c06c753fed8a6586daefced22bc897381ba394eb2b56fb65984df12adf18343c0657e3b7d85407b735d52c'
