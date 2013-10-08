533be4b4a554b1c6958081ff799e03a515ae4e4e
Yune Kotomi <yune-kotomi@users.noreply.github.com>
2013-10-09 08:29:42 +0900

    プロフィール更新処理を新仕様に

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 require "digest/sha2"
 require "timeout"
 require "net/http"
@@ -38,24 +39,27 @@ class Service < ActiveRecord::Base
   end
 
   def notice(profile)
-    data = ({
-        :profile_id => profile.id,
-        :nickname => profile.nickname,
-        :profile_text => 
-          profile.profile_html(:section_anchor_prefix => "profile_")
-      }).to_json
-    timestamp = Time.now.to_i
-    signature = sign(:data => data, :timestamp => timestamp)
+    params = {
+      'id' => self.id,
+      'profile_id' => profile.id,
+      'nickname' => profile.nickname,
+      # cf. http://jasonfox.com/post/14571407609/undefined-method-bytesize-for-nil-nilclass
+      'profile_text' => ActionController::Base.helpers.strip_tags(profile.profile_html(:section_anchor_prefix => "profile_")),
+      'timestamp' => Time.now.to_i
+    }
 
+    message = [
+      params['id'], params['profile_id'], 
+      params['nickname'], params['profile_text'], params['timestamp']
+    ].join
+    params['signature'] = OpenSSL::HMAC::hexdigest(OpenSSL::Digest::SHA256.new, self.key, message)
+
+    params = params.map {|key, value| "#{key}=#{CGI.escape(value.to_s)}" }.join("&")
     begin
       timeout(3) do
         uri = URI(self.profile_update)
         Net::HTTP.start(uri.host, uri.port) {|http|
-          http.post(
-            uri.path, 
-            "data=#{URI.escape data}&timestamp=#{timestamp}" + 
-              "&sign=#{URI.escape signature}"
-          )
+          http.post(uri.path, params)
         }
       end
     rescue Exception => e
