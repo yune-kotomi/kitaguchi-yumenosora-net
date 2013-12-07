2ec988884dd4b174fbbb041ff7f6adfbbef1863f
Yune Kotomi <yune-kotomi@users.noreply.github.com>
2013-12-07 17:22:45 +0900

    ActionController::RoutingErrorでのエラーログを抑制

---
+++ config/initializers/ignore_routing_error.rb
@@ -0,0 +1,16 @@
+# Be sure to restart your server when you modify this file.
+
+case Rails.env
+when 'production'
+  class ActionDispatch::DebugExceptions
+    alias_method :old_log_error, :log_error
+    def log_error(env, wrapper)
+      if wrapper.exception.is_a?  ActionController::RoutingError
+        return
+      else
+        old_log_error env, wrapper
+      end
+    end
+  end
+end
+
