1a18ba60b034673412b7d4a94bf67086cdf74485
Yune Kotomi <yune-kotomi@users.noreply.github.com>
2014-03-21 13:20:35 +0900

    プロフィール編集画面からの戻りリンクの自由度向上

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,2 +1,8 @@
 module ProfilesHelper
+  def back_from_profile(service, profile)
+    service.back_from_profile.
+      gsub('PROFILE_ID', profile.id.to_s).
+      gsub('DOMAIN_NAME', profile.domain_name).
+      gsub('SCREEN_NAME', profile.screen_name)
+  end
 end
